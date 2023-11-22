8de69bd57d006f0043716c6918ffa78cd7761dc9
cherif diallo <riftone07@gmail.com>
2023-11-22 06:42:36 +0000

    changement recouvrement

--- app/Http/Controllers/API/EcheanceController.php
+++ app/Http/Controllers/API/EcheanceController.php
@@ -19,7 +19,7 @@ class EcheanceController extends Controller
         $startDate = $request->input('start_date');
         $endDate = $request->input('end_date');
 
-        $echeances = Echeance::whereBetween('DO_Date', [$startDate, $endDate])->orderBy('DO_Date','desc')->orderBy('DO_Piece','DESC')->paginate(150);
+        $echeances = Echeance::whereBetween('DO_Date', [$startDate, $endDate])->orderBy('DO_Date','desc')->orderBy('DO_Piece','DESC')->get();
 
         return $echeances;
     }
