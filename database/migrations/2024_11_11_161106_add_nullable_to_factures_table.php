95d4f01d985472ec861ce29ae1830e29521ce710
Cherif Diallo <riftone07@gmail.com>
2024-11-11 16:22:34 +0000

    chns

---
+++ database/migrations/2024_11_11_161106_add_nullable_to_factures_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('factures', function (Blueprint $table) {
+            $table->string('NUM_PIECE',13)->change();
+            $table->string('CG_NUM',13)->nullable()->change();
+        });
+    }
+
+};
